0c66b64e56738b7eb391d2766fa37c59a678406b
DavidSucias <david.sucias@gft.com>
2021-07-08 15:12:27 +0200

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Sucias Colomer, David on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
